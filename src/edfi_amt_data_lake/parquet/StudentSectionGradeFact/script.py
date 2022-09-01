072ceed4f787386aad7cc099df497493364cd54c
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2022-09-01 17:31:10 -0600

    Generates parquet file for StudentSectionGradeFact view.

---
+++ src/edfi_amt_data_lake/parquet/StudentSectionGradeFact/script.py
@@ -0,0 +1,126 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+from distutils.util import subst_vars
+from operator import contains
+from decouple import config
+from edfi_amt_data_lake.parquet.Common.functions import getEndpointJson
+from edfi_amt_data_lake.parquet.Common.pandasWrapper import jsonNormalize, pdMerge, subset, renameColumns, saveParquetFile, addColumnIfNotExists, toCsv
+
+ENDPOINT_GRADES = 'grades'
+GRANDINGPERIODS_GRADES = 'gradingPeriods'
+
+def StudentSectionGradeFact(school_year="") -> None:
+    gradesContent = getEndpointJson(ENDPOINT_GRADES, config('SILVER_DATA_LOCATION'), school_year)
+    gradingPeriodsContent = getEndpointJson(GRANDINGPERIODS_GRADES, config('SILVER_DATA_LOCATION'), school_year)
+
+    gradesContentNormalized =  jsonNormalize(
+        gradesContent,
+        None,
+        None,
+        None,
+        None,
+        'ignore'
+    )
+
+    gradingPeriodsContentNormalized =  jsonNormalize(
+        gradingPeriodsContent,
+        None,
+        None,
+        None,
+        None,
+        'ignore'
+    )
+
+    restultDataFrame = pdMerge(
+        left=gradesContentNormalized, 
+        right=gradingPeriodsContentNormalized,
+        how='left',
+        leftOn=['gradingPeriodReference.gradingPeriodDescriptor','gradingPeriodReference.periodSequence','gradingPeriodReference.schoolId','gradingPeriodReference.schoolYear'],
+        rigthOn=['gradingPeriodDescriptor','periodSequence','schoolReference.schoolId','schoolYearTypeReference.schoolYear'],
+        suffixLeft='',
+        suffixRight='_gradingPeriods'
+    )
+
+    toCsv(restultDataFrame, 'C:\\temp\\edfi\\studentSectionGradeFact.csv')
+
+    restultDataFrame = subset(restultDataFrame, [
+        'studentSectionAssociationReference.studentUniqueId',
+        'studentSectionAssociationReference.schoolId',
+        'gradingPeriodReference.gradingPeriodDescriptor',
+        'beginDate',
+        'studentSectionAssociationReference.localCourseCode',
+        'studentSectionAssociationReference.schoolYear',
+        'studentSectionAssociationReference.sectionIdentifier',
+        'studentSectionAssociationReference.sessionName',
+        'numericGradeEarned',
+        'letterGradeEarned',
+        'gradeTypeDescriptor'
+    ])
+
+    # Removes namespace from Grading Period Descriptor
+    if not restultDataFrame['gradingPeriodReference.gradingPeriodDescriptor'].empty:
+        if len(restultDataFrame['gradingPeriodReference.gradingPeriodDescriptor'].str.split('#')) > 0:
+            restultDataFrame["gradingPeriodReference.gradingPeriodDescriptor"] = restultDataFrame["gradingPeriodReference.gradingPeriodDescriptor"].str.split("#").str.get(1)
+
+    # Removes namespace from Grade Type Descriptor
+    if not restultDataFrame['gradeTypeDescriptor'].empty:
+        if len(restultDataFrame['gradeTypeDescriptor'].str.split('#')) > 0:
+            restultDataFrame["gradeTypeDescriptor"] = restultDataFrame["gradeTypeDescriptor"].str.split("#").str.get(1)
+
+    # Converting some fields to str as preparation for the parquet file.
+    restultDataFrame['studentSectionAssociationReference.schoolId'] = restultDataFrame['studentSectionAssociationReference.schoolId'].astype(str)
+    restultDataFrame['studentSectionAssociationReference.schoolYear'] = restultDataFrame['studentSectionAssociationReference.schoolYear'].astype(str)
+
+    # Creates concatanation for GradingPeriodKey field
+    restultDataFrame['GradingPeriodKey'] = (
+            restultDataFrame['gradingPeriodReference.gradingPeriodDescriptor']
+            + ', ' + restultDataFrame['studentSectionAssociationReference.schoolId']
+            + ' ' + restultDataFrame['beginDate']
+        )
+
+    # Creates concatanation for GradingPeriodKey field
+    restultDataFrame['StudentSectionKey'] = (
+            restultDataFrame['studentSectionAssociationReference.studentUniqueId']
+            + ', ' + restultDataFrame['studentSectionAssociationReference.schoolId']
+            + ' ' + restultDataFrame['studentSectionAssociationReference.localCourseCode']
+            + ' ' + restultDataFrame['studentSectionAssociationReference.schoolYear']
+            + ' ' + restultDataFrame['studentSectionAssociationReference.sectionIdentifier']
+            + ' ' + restultDataFrame['studentSectionAssociationReference.sessionName']
+            + ' ' + restultDataFrame['beginDate']
+        )
+
+    # Creates concatanation for SectionKey field
+    restultDataFrame['SectionKey'] = (
+            restultDataFrame['studentSectionAssociationReference.schoolId']
+            + ' ' + restultDataFrame['studentSectionAssociationReference.localCourseCode']
+            + ' ' + restultDataFrame['studentSectionAssociationReference.schoolYear']
+            + ' ' + restultDataFrame['studentSectionAssociationReference.sectionIdentifier']
+            + ' ' + restultDataFrame['studentSectionAssociationReference.sessionName']
+        )
+
+    # Rename columns to match AMT
+    restultDataFrame = renameColumns(restultDataFrame, 
+        {
+            'studentSectionAssociationReference.studentUniqueId': 'StudentKey',
+            'studentSectionAssociationReference.schoolId': 'SchoolKey',
+            'numericGradeEarned': 'NumericGradeEarned',
+            'letterGradeEarned': 'LetterGradeEarned',
+            'gradeTypeDescriptor': 'GradeType'
+        })
+
+    # Reorder columns to match AMT
+    restultDataFrame = restultDataFrame[[
+            'StudentKey',
+            'SchoolKey',
+            'GradingPeriodKey',
+            'StudentSectionKey',
+            'SectionKey',
+            'NumericGradeEarned',
+            'LetterGradeEarned',
+            'GradeType'
+        ]]
+
+    toCsv(restultDataFrame, 'C:\\temp\\edfi\\studentSectionGradeFact2.csv')
