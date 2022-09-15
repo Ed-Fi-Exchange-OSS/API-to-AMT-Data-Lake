a5fe5e4ba4ac56517d5f7a5f4e0afb7c5a8dd0f1
José Alberto Leiva Obando <56046999+jleiva-gap@users.noreply.github.com>
2022-09-15 10:45:20 -0600

    [BIA-1173] Implement API to AMT parquet conversion - student equity - StudentDisciplineActionDim (#24)

--- src/edfi_amt_data_lake/parquet/amt/equity/equity_collection.py
+++ src/edfi_amt_data_lake/parquet/amt/equity/equity_collection.py
@@ -2,9 +2,11 @@
 # Licensed to the Ed-Fi Alliance under one or more agreements.
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
+from edfi_amt_data_lake.parquet.amt.equity.student_discipline_action_dim.main import student_discipline_action_dim
 from edfi_amt_data_lake.parquet.amt.equity.feeder_school_dim.main import feeder_school_dim
 from edfi_amt_data_lake.parquet.amt.equity.StudentSchoolFoodServiceProgramDim.script import studentSchoolFoodServiceProgramDim
 
 def equity_collection(school_year) -> None:
+    student_discipline_action_dim(school_year)
     feeder_school_dim(school_year)
     studentSchoolFoodServiceProgramDim(school_year)
