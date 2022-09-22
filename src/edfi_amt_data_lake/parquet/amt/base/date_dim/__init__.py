566766488562dd1fe841ca55589f447ee1fb130f
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2022-09-22 10:52:56 -0600

    [BIA-1195] Implement API to AMT parquet conversion - core collection - DateDim
    Add datedim view

--- src/edfi_amt_data_lake/parquet/amt/base/date_dim/__init__.py
+++ src/edfi_amt_data_lake/parquet/amt/base/date_dim/__init__.py
@@ -2,10 +2,3 @@
 # Licensed to the Ed-Fi Alliance under one or more agreements.
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
-
-from edfi_amt_data_lake.parquet.amt.base.date_dim.main import date_dim
-from edfi_amt_data_lake.parquet.amt.base.SchoolDim.script import schoolDim
-
-def base_collection(school_year) -> None:
-    date_dim(school_year)
-    schoolDim(school_year)
