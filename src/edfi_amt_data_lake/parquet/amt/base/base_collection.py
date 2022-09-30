7fd462b0f3fc47672696080d3ca1dc483a93bafd
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2022-09-30 09:57:53 -0600

    Implement API to AMT parquet conversion - MostRecentGradingPeriod

--- src/edfi_amt_data_lake/parquet/amt/base/base_collection.py
+++ src/edfi_amt_data_lake/parquet/amt/base/base_collection.py
@@ -7,10 +7,14 @@ from edfi_amt_data_lake.parquet.amt.base.date_dim.main import date_dim
 from edfi_amt_data_lake.parquet.amt.base.grading_period_dim.main import (
     grading_period_dim,
 )
+from edfi_amt_data_lake.parquet.amt.base.most_recent_grading_period.main import (
+    most_recent_grading_period,
+)
 from edfi_amt_data_lake.parquet.amt.base.school_dim.main import school_dim
 
 
 def base_collection(school_year) -> None:
     date_dim(school_year)
     grading_period_dim(school_year)
+    most_recent_grading_period(school_year)
     school_dim(school_year)
