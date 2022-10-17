bca5ec0b9ce4a2d9f0ca8124a0529b53cb30ac95
José Alberto Leiva Obando <56046999+jleiva-gap@users.noreply.github.com>
2022-10-16 18:49:43 -0600

    [BIA-1197] Implement API to AMT - epp_CandidateSurveyDim (#33)
    - Update workflow to have to steps: get_api_data and generate_parquet.
    - Add candidate_survey_dim.

--- src/dagster_data_lake/dagster_data_lake/jobs/pipe_api.py
+++ src/dagster_data_lake/dagster_data_lake/jobs/pipe_api.py
@@ -1,8 +1,13 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
 from dagster import job
 
-from dagster_data_lake.ops.api import api
+from dagster_data_lake.ops.api import generate_parquet, get_api_data
 
 
 @job
 def pipe_api_job():
-    api()
+    generate_parquet(get_api_data())
--- src/dagster_data_lake/dagster_data_lake/ops/api.py
+++ src/dagster_data_lake/dagster_data_lake/ops/api.py
@@ -1,3 +1,8 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
 from dagster import op
 
 from edfi_amt_data_lake.api.api import get_all
@@ -7,8 +12,15 @@ from edfi_amt_data_lake.parquet.amt_parquet import generate_amt_parquet
 
 
 @op
-def api() -> None:
+def get_api_data() -> None:
     for school_year in get_school_year():
         if get_change_version_updated(school_year):
             get_all(school_year)
-        generate_amt_parquet(school_year)
+    return True
+
+
+@op
+def generate_parquet(api_result_sucess) -> None:
+    if(api_result_sucess):
+        for school_year in get_school_year():
+            generate_amt_parquet(school_year)
