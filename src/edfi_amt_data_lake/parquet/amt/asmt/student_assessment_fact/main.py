b8b533ecdc8e650631f40d530cdcf682c2569411
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2022-11-03 16:01:05 -0600

    [BIA-1232] Optional data StudentAssessmentFact
    * Update jsonNormalize to create a default dataframe y return a subset of columns.
    * Add recordMeta parameter to set recordPath columns
    * Update getEndpointJson to return empty if no json files founded.
    * Update student_assessment_fact to send all the columns in the meta parameter.

--- src/edfi_amt_data_lake/parquet/amt/asmt/student_assessment_fact/main.py
+++ src/edfi_amt_data_lake/parquet/amt/asmt/student_assessment_fact/main.py
@@ -9,6 +9,7 @@ from decouple import config
 
 from edfi_amt_data_lake.parquet.Common.functions import getEndpointJson
 from edfi_amt_data_lake.parquet.Common.pandasWrapper import (
+    add_dataframe_column,
     get_descriptor_code_value_from_uri,
     jsonNormalize,
     pdMerge,
@@ -22,20 +23,27 @@ ENDPOINT_STUDENT_ASSESSSMENTS = "studentAssessments"
 ENDPOINT_STUDENT_SCHOOL_ASSOCIATION = "studentSchoolAssociations"
 
 
-def student_assessment_fact(school_year) -> None:
+def student_assessment_fact_dataframe(school_year) -> None:
     silverDataLocation = config("SILVER_DATA_LOCATION")
     student_assessment_json = getEndpointJson(ENDPOINT_STUDENT_ASSESSSMENTS, silverDataLocation, school_year)
     student_school_association_json = getEndpointJson(ENDPOINT_STUDENT_SCHOOL_ASSOCIATION, silverDataLocation, school_year)
-
+    ############################
+    # studentSchoolAssociation
+    ############################
     student_school_association_content = jsonNormalize(
         student_school_association_json,
         recordPath=None,
-        meta=[],
+        meta=[
+            'id',
+            'entryDate',
+            'schoolReference.schoolId',
+            'studentReference.studentUniqueId',
+            'exitWithdrawDate'
+        ],
         metaPrefix=None,
         recordPrefix=None,
         errors="ignore",
     )
-
     student_school_association_content = subset(student_school_association_content, [
         'id',
         'entryDate',
@@ -43,73 +51,112 @@ def student_assessment_fact(school_year) -> None:
         'studentReference.studentUniqueId',
         'exitWithdrawDate'
     ])
-
-    student_assesssment_content = jsonNormalize(
+    ############################
+    # student assessment
+    ############################
+    student_assessment_content = jsonNormalize(
         student_assessment_json,
         recordPath=None,
-        meta=[],
+        meta=[
+            'id',
+            'administrationDate',
+            'studentAssessmentIdentifier',
+            'assessmentReference.assessmentIdentifier',
+            'assessmentReference.namespace',
+            'studentReference.studentUniqueId',
+            'objectiveAssessmentReference.identificationCode'
+        ],
         metaPrefix=None,
         recordPrefix=None,
         errors="ignore",
     )
-
+    ############################
+    # studentObjectiveAssessments
+    ############################
     student_objective_assessment = jsonNormalize(
         student_assessment_json,
-        recordPath=['studentObjectiveAssessments'],
+        recordPath=[
+            'studentObjectiveAssessments'
+        ],
         meta=['id'],
         metaPrefix=None,
+        recordMeta=[
+            'objectiveAssessmentReference.identificationCode',
+        ],
         recordPrefix=None,
         errors='ignore'
     )
-
     student_assessment_score_results = jsonNormalize(
         student_assessment_json,
         recordPath=['scoreResults'],
         meta=['id'],
         metaPrefix=None,
+        recordMeta=[
+            'assessmentReportingMethodDescriptor',
+            'result',
+            'resultDatatypeTypeDescriptor'
+        ],
         recordPrefix=None,
         errors='ignore'
     )
-
     student_assessment_performance_levels = jsonNormalize(
         student_assessment_json,
         recordPath=['performanceLevels'],
         meta=['id'],
         metaPrefix=None,
+        recordMeta=[
+            'assessmentReportingMethodDescriptor',
+            'performanceLevelDescriptor'
+            'performanceLevelMet'
+        ],
         recordPrefix=None,
         errors='ignore'
     )
-
     student_objective_assessment_scoreResults = jsonNormalize(
         student_assessment_json,
         recordPath=['studentObjectiveAssessments', 'scoreResults'],
         meta=[
-            "id",
+            'id',
             ['studentObjectiveAssessments', 'objectiveAssessmentReference', 'identificationCode']
         ],
-        metaPrefix=None,
+        recordMeta=[
+            'assessmentReportingMethodDescriptor',
+            'result',
+            'resultDatatypeTypeDescriptor'
+        ],
         recordPrefix=None,
         errors='ignore'
     )
-
+    student_objective_assessment_scoreResults = add_dataframe_column(
+        student_objective_assessment_scoreResults,
+        [
+            'studentObjectiveAssessments.objectiveAssessmentReference.identificationCode'
+        ]
+    )
     student_objective_assessment_performanceLevels = jsonNormalize(
         student_assessment_json,
         recordPath=['studentObjectiveAssessments', 'performanceLevels'],
         meta=[
-            "id",
+            'id',
             ['studentObjectiveAssessments', 'objectiveAssessmentReference', 'identificationCode']
         ],
         metaPrefix=None,
+        recordMeta=[
+            'assessmentReportingMethodDescriptor',
+            'performanceLevelDescriptor',
+            'performanceLevelMet'
+        ],
         recordPrefix=None,
         errors='ignore'
     )
-
-    data_frame = student_assesssment_content
-
-    data_frame['s_score_results'] = '|'
-
+    student_objective_assessment_performanceLevels = add_dataframe_column(
+        student_objective_assessment_performanceLevels,
+        [
+            'studentObjectiveAssessments.objectiveAssessmentReference.identificationCode'
+        ]
+    )
     data_frame = pdMerge(
-        left=data_frame,
+        left=student_assessment_content,
         right=student_assessment_score_results,
         how='left',
         leftOn=['id'],
@@ -117,9 +164,6 @@ def student_assessment_fact(school_year) -> None:
         suffixLeft=None,
         suffixRight=None
     )
-
-    data_frame['s_performance_levels'] = '|'
-
     data_frame = pdMerge(
         left=data_frame,
         right=student_assessment_performance_levels,
@@ -129,9 +173,6 @@ def student_assessment_fact(school_year) -> None:
         suffixLeft=None,
         suffixRight='_performance_levels'
     )
-
-    data_frame['s_objective_assessment'] = '|'
-
     data_frame = pdMerge(
         left=data_frame,
         right=student_objective_assessment,
@@ -141,9 +182,12 @@ def student_assessment_fact(school_year) -> None:
         suffixLeft=None,
         suffixRight='_student_objective'
     )
-
-    data_frame['s_objective_performance_levels'] = '|'
-
+    student_objective_assessment_performanceLevels = add_dataframe_column(
+        student_objective_assessment_performanceLevels,
+        [
+            'objectiveAssessmentReference.identificationCode'
+        ]
+    )
     data_frame = pdMerge(
         left=data_frame,
         right=student_objective_assessment_performanceLevels,
@@ -159,9 +203,6 @@ def student_assessment_fact(school_year) -> None:
         suffixLeft=None,
         suffixRight='_student_objective_performanceLevels'
     )
-
-    data_frame['s_objective_assessment_scoreResults'] = '|'
-
     data_frame = pdMerge(
         left=data_frame,
         right=student_objective_assessment_scoreResults,
@@ -177,9 +218,7 @@ def student_assessment_fact(school_year) -> None:
         suffixLeft=None,
         suffixRight='_student_objective_scoreResults'
     )
-
     data_frame['s_student_school_association_content'] = '|'
-
     data_frame = pdMerge(
         left=data_frame,
         right=student_school_association_content,
@@ -189,30 +228,19 @@ def student_assessment_fact(school_year) -> None:
         suffixLeft=None,
         suffixRight='_student_school_association'
     )
-
     data_frame['exitWithdrawDate'] = to_datetime_key(data_frame, 'exitWithdrawDate')
     data_frame['date_now'] = date.today()
     data_frame['date_now'] = to_datetime_key(data_frame, 'date_now')
     data_frame = data_frame[data_frame['exitWithdrawDate'] >= data_frame['date_now']]
-
     get_descriptor_code_value_from_uri(data_frame, 'whenAssessedGradeLevelDescriptor')
-
     get_descriptor_code_value_from_uri(data_frame, 'assessmentReportingMethodDescriptor')
-
     get_descriptor_code_value_from_uri(data_frame, 'resultDatatypeTypeDescriptor')
-
     get_descriptor_code_value_from_uri(data_frame, 'assessmentReportingMethodDescriptor_performance_levels')
-
     get_descriptor_code_value_from_uri(data_frame, 'performanceLevelDescriptor')
-
     get_descriptor_code_value_from_uri(data_frame, 'assessmentReportingMethodDescriptor_student_objective_performanceLevels')
-
     get_descriptor_code_value_from_uri(data_frame, 'performanceLevelDescriptor_student_objective_performanceLevels')
-
     get_descriptor_code_value_from_uri(data_frame, 'assessmentReportingMethodDescriptor_student_objective_scoreResults')
-
     get_descriptor_code_value_from_uri(data_frame, 'resultDatatypeTypeDescriptor_student_objective_scoreResults')
-
     data_frame = subset(data_frame, [
         'id',
         'studentAssessmentIdentifier',
@@ -221,28 +249,21 @@ def student_assessment_fact(school_year) -> None:
         'assessmentReference.assessmentIdentifier',
         'assessmentReference.namespace',
         'studentReference.studentUniqueId',
-        's_objective_assessment',
         'objectiveAssessmentReference.identificationCode',
-        's_score_results',
         'assessmentReportingMethodDescriptor',
         'result',
         'resultDatatypeTypeDescriptor',
-        's_performance_levels',
         'assessmentReportingMethodDescriptor_performance_levels',
         'performanceLevelDescriptor',
-        's_objective_performance_levels',
         'assessmentReportingMethodDescriptor_student_objective_performanceLevels',
         'performanceLevelDescriptor_student_objective_performanceLevels',
-        's_objective_assessment_scoreResults',
         'assessmentReportingMethodDescriptor_student_objective_scoreResults',
         'result_student_objective_scoreResults',
         'resultDatatypeTypeDescriptor_student_objective_scoreResults',
-        's_student_school_association_content',
         'entryDate',
         'schoolReference.schoolId',
         'exitWithdrawDate'
     ])
-
     data_frame['StudentAssessmentFactKey'] = (
         data_frame['assessmentReference.assessmentIdentifier'] + '-'
         + data_frame['assessmentReference.namespace'] + '-'
@@ -256,24 +277,20 @@ def student_assessment_fact(school_year) -> None:
         + data_frame['schoolReference.schoolId'].astype(str) + '-'
         + data_frame['entryDate'].astype(str)
     )
-
     data_frame['StudentAssessmentKey'] = (
         data_frame['assessmentReference.assessmentIdentifier'] + '-'
         + data_frame['assessmentReference.namespace'] + '-'
         + data_frame['studentAssessmentIdentifier'] + '-'
         + data_frame['studentReference.studentUniqueId']
     )
-
     data_frame['AssessmentKey'] = (
         data_frame['assessmentReference.assessmentIdentifier'] + '-'
         + data_frame['assessmentReference.namespace']
     )
-
     data_frame['StudentSchoolKey'] = (
         data_frame['studentReference.studentUniqueId'] + '-'
         + data_frame['schoolReference.schoolId'].astype(str)
     )
-
     data_frame['StudentObjectiveAssessmentKey'] = (
         data_frame['studentReference.studentUniqueId'] + '-'
         + data_frame['objectiveAssessmentReference.identificationCode'] + '-'
@@ -281,13 +298,11 @@ def student_assessment_fact(school_year) -> None:
         + data_frame['studentAssessmentIdentifier'] + '-'
         + data_frame['assessmentReference.namespace']
     )
-
     data_frame['ObjectiveAssessmentKey'] = (
         + data_frame['assessmentReference.assessmentIdentifier'] + '-'
         + data_frame['objectiveAssessmentReference.identificationCode'] + '-'
         + data_frame['assessmentReference.namespace']
     )
-
     data_frame = renameColumns(data_frame, {
         'assessmentReference.assessmentIdentifier': 'AssessmentIdentifier',
         'assessmentReference.namespace': 'Namespace',
@@ -301,19 +316,17 @@ def student_assessment_fact(school_year) -> None:
         'assessmentReportingMethodDescriptor_student_objective_scoreResults': 'ReportingMethod',
         'performanceLevelDescriptor_student_objective_performanceLevels': 'PerformanceResult'
     })
-
     data_frame = data_frame.fillna('')
-
+    # Result columns
     data_frame.loc[data_frame['StudentScore'] == '', 'StudentScore'] = data_frame['result']
     data_frame.loc[data_frame['ResultDataType'] == '', 'ResultDataType'] = data_frame['resultDatatypeTypeDescriptor']
     data_frame.loc[data_frame['ReportingMethod'] == '', 'ReportingMethod'] = data_frame['assessmentReportingMethodDescriptor']
     data_frame.loc[data_frame['PerformanceResult'] == '', 'PerformanceResult'] = data_frame['performanceLevelDescriptor']
-
     data_frame["StudentAssessmentScore"] = data_frame['result']
     data_frame["StudentAssessmentResultDataType"] = data_frame['resultDatatypeTypeDescriptor']
     data_frame["StudentAssessmentReportingMethod"] = data_frame['assessmentReportingMethodDescriptor']
     data_frame["StudentAssessmentPerformanceResult"] = data_frame['performanceLevelDescriptor']
-
+    # Result dataframe
     data_frame = data_frame[[
         'StudentAssessmentFactKey',
         'StudentAssessmentKey',
@@ -337,5 +350,9 @@ def student_assessment_fact(school_year) -> None:
         'StudentAssessmentReportingMethod',
         'StudentAssessmentPerformanceResult'
     ]]
+    return data_frame
 
-    saveParquetFile(data_frame, f"{config('PARQUET_FILES_LOCATION')}", "asmt_student_assessment_fact.parquet", school_year)
+
+def student_assessment_fact(school_year) -> None:
+    result_data_frame = student_assessment_fact_dataframe(school_year)
+    saveParquetFile(result_data_frame, f"{config('PARQUET_FILES_LOCATION')}", "asmt_student_assessment_fact.parquet", school_year)
