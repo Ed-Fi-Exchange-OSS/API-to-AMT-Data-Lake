da756555a446f1e74f04d0e068443e15f941ddfc
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2022-11-22 11:42:39 -0600

    Replaces NA with Not applicable on LimitedEnglishProficiency column.

--- src/edfi_amt_data_lake/parquet/amt/base/all_student_school_dim/main.py
+++ src/edfi_amt_data_lake/parquet/amt/base/all_student_school_dim/main.py
@@ -22,6 +22,8 @@ from edfi_amt_data_lake.parquet.Common.pandasWrapper import (
     renameColumns,
     subset,
     to_datetime_key,
+    toCsv,
+    replace_null,
 )
 
 ENDPOINT_STUDENT_SCHOOL_ASSOCIATIONS = 'studentSchoolAssociations'
@@ -164,8 +166,14 @@ def all_student_school_dim_data_frame_base(
             errors='ignore'
         )
 
+        toCsv(student_school_education_organization_associations_normalized, 'C:/temp/edfi/parquet/', 'student_school_dim2.csv', '')
+
+        replace_null(student_school_education_organization_associations_normalized, 'limitedEnglishProficiencyDescriptor', '')
+
         get_descriptor_code_value_from_uri(student_school_education_organization_associations_normalized, 'limitedEnglishProficiencyDescriptor')
         get_descriptor_code_value_from_uri(student_school_education_organization_associations_normalized, 'sexDescriptor')
+
+        toCsv(student_school_education_organization_associations_normalized, 'C:/temp/edfi/parquet/', 'student_school_dim3.csv', '')
 
         student_school_education_organization_associations_indicators_normalized = jsonNormalize(
             data=student_school_education_organization_associations_content,
@@ -301,6 +309,8 @@ def all_student_school_dim_data_frame_base(
             errors='ignore'
         )
 
+        replace_null(student_school_education_organization_associations_district_normalized, 'limitedEnglishProficiencyDescriptor', '')
+
         get_descriptor_code_value_from_uri(student_school_education_organization_associations_district_normalized, 'limitedEnglishProficiencyDescriptor')
         get_descriptor_code_value_from_uri(student_school_education_organization_associations_district_normalized, 'sexDescriptor')
 
@@ -411,6 +421,8 @@ def all_student_school_dim_data_frame_base(
     # District Education Organization ends
 
     result_data_frame = result_data_frame.fillna('')
+
+    toCsv(result_data_frame, 'C:/temp/edfi/parquet/', 'student_school_dim1.csv', '')
 
     # LimitedEnglishProficiency
     result_data_frame['LimitedEnglishProficiency'] = (
