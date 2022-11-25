bd98e8a6056c2ee5775c0c93c3370160158d7591
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2022-11-25 10:43:32 -0600

    [BIA-1187] - Implement API to AMT parquet conversion - Student School Dim. (#59)
    * Implement API to AMT parquet conversion - Student School Dim.

--- src/edfi_amt_data_lake/parquet/amt/base/base_collection.py
+++ src/edfi_amt_data_lake/parquet/amt/base/base_collection.py
@@ -6,7 +6,7 @@
 
 from edfi_amt_data_lake.parquet.amt.base.all_student_school_dim.main import (
     all_student_school_dim,
-) 
+)
 from edfi_amt_data_lake.parquet.amt.base.class_period_dim.main import class_period_dim
 from edfi_amt_data_lake.parquet.amt.base.contact_person_dim.main import (
     contact_person_dim,
@@ -33,6 +33,9 @@ from edfi_amt_data_lake.parquet.amt.base.student_program_dim.main import (
 from edfi_amt_data_lake.parquet.amt.base.student_school_demographics_bridge.main import (
     student_school_demographics_bridge,
 )
+from edfi_amt_data_lake.parquet.amt.base.student_school_dim.main import (
+    student_school_dim,
+)
 from edfi_amt_data_lake.parquet.amt.base.student_section_dim.main import (
     student_section_dim,
 )
@@ -53,3 +56,4 @@ def base_collection(school_year) -> None:
     student_local_education_agency_demographics_bridge(school_year)
     student_school_demographics_bridge(school_year)
     student_section_dim(school_year)
+    student_school_dim(school_year)
---
+++ src/edfi_amt_data_lake/parquet/amt/base/student_school_dim/main.py
@@ -0,0 +1,89 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+import pandas as pd
+
+from edfi_amt_data_lake.helper.data_frame_generation_result import (
+    data_frame_generation_result,
+)
+from edfi_amt_data_lake.parquet.amt.base.all_student_school_dim.main import (
+    all_student_school_dim_data_frame_base,
+)
+from edfi_amt_data_lake.parquet.Common.pandasWrapper import create_parquet_file
+
+RESULT_COLUMNS = [
+    'StudentSchoolKey',
+    'StudentKey',
+    'SchoolKey',
+    'SchoolYear',
+    'StudentFirstName',
+    'StudentMiddleName',
+    'StudentLastName',
+    'BirthDate',
+    'EnrollmentDateKey',
+    'GradeLevel',
+    'LimitedEnglishProficiency',
+    'IsHispanic',
+    'Sex',
+    'InternetAccessInResidence',
+    'InternetAccessTypeInResidence',
+    'InternetPerformance',
+    'DigitalDevice',
+    'DeviceAccess'
+]
+
+
+@create_parquet_file
+def student_school_dim_data_frame(
+    file_name: str,
+    columns: list[str],
+    school_year: int
+) -> pd.DataFrame:
+    file_name = file_name
+
+    all_student_school_dim_columns = [
+        'AllStudentSchoolKey',
+        'StudentSchoolKey',
+        'StudentKey',
+        'SchoolKey',
+        'SchoolYear',
+        'StudentFirstName',
+        'StudentMiddleName',
+        'StudentLastName',
+        'BirthDate',
+        'EnrollmentDateKey',
+        'GradeLevel',
+        'LimitedEnglishProficiency',
+        'IsHispanic',
+        'Sex',
+        'InternetAccessInResidence',
+        'InternetAccessTypeInResidence',
+        'InternetPerformance',
+        'DigitalDevice',
+        'DeviceAccess',
+        'IsEnrolled',
+        'ExitWithdrawDate'
+    ]
+
+
+    result_data_frame = all_student_school_dim_data_frame_base(
+        file_name=file_name,
+        columns=all_student_school_dim_columns,
+        school_year=school_year
+    )
+
+    result_data_frame = result_data_frame[result_data_frame['IsEnrolled'] == 1]
+
+    return result_data_frame[
+        columns
+    ]
+
+
+def student_school_dim(school_year) -> data_frame_generation_result:
+    return student_school_dim_data_frame(
+        file_name="studentSchoolDim.parquet",
+        columns=RESULT_COLUMNS,
+        school_year=school_year
+    )
