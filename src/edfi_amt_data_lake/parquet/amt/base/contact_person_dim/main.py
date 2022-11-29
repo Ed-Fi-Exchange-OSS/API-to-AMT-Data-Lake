76d943fccb69f17875febf36efd737fae2c78637
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2022-11-29 17:13:28 -0600

    Consider optional data - ContactPersonDim.

--- src/edfi_amt_data_lake/parquet/amt/base/contact_person_dim/main.py
+++ src/edfi_amt_data_lake/parquet/amt/base/contact_person_dim/main.py
@@ -4,28 +4,59 @@
 # See the LICENSE and NOTICES files in the project root for more information.
 
 from datetime import date
-
+from edfi_amt_data_lake.helper.data_frame_generation_result import (
+    data_frame_generation_result,
+)
 import pandas as pd
 from decouple import config
 
 from edfi_amt_data_lake.parquet.Common.descriptor_mapping import get_descriptor_constant
 from edfi_amt_data_lake.parquet.Common.functions import getEndpointJson
 from edfi_amt_data_lake.parquet.Common.pandasWrapper import (
+    create_parquet_file,
     addColumnIfNotExists,
     get_descriptor_code_value_from_uri,
     jsonNormalize,
     pdMerge,
     renameColumns,
-    saveParquetFile,
     subset,
     to_datetime_key,
+    toCsv
 )
 
 ENDPOINT_STUDENT_PARENT_ASSOCIATIONS = 'studentParentAssociations'
 ENDPOINT_PARENTS = 'parents'
-
-
-def contact_person_dim_dataframe(school_year) -> pd.DataFrame:
+RESULT_COLUMNS = [
+    'UniqueKey',
+    'ContactPersonKey',
+    'StudentKey',
+    'ContactFirstName',
+    'ContactLastName',
+    'RelationshipToStudent',
+    'ContactHomeAddress',
+    'ContactPhysicalAddress',
+    'ContactMailingAddress',
+    'ContactWorkAddress',
+    'ContactTemporaryAddress',
+    'HomePhoneNumber',
+    'MobilePhoneNumber',
+    'WorkPhoneNumber',
+    'PrimaryEmailAddress',
+    'PersonalEmailAddress',
+    'WorkEmailAddress',
+    'IsPrimaryContact',
+    'StudentLivesWith',
+    'IsEmergencyContact',
+    'ContactPriority',
+    'ContactRestrictions',
+    'PostalCode'
+]
+@create_parquet_file
+def contact_person_dim_dataframe(
+    file_name: str,
+    columns: list[str],
+    school_year: int
+):
     student_parent_associations_content = getEndpointJson(ENDPOINT_STUDENT_PARENT_ASSOCIATIONS, config('SILVER_DATA_LOCATION'), school_year)
     parents_content = getEndpointJson(ENDPOINT_PARENTS, config('SILVER_DATA_LOCATION'), school_year)
 
@@ -77,6 +108,14 @@ def contact_person_dim_dataframe(school_year) -> pd.DataFrame:
         meta=[
             'id'
         ],
+        recordMeta=[
+            'addressTypeDescriptor',
+            'city',
+            'postalCode',
+            'stateAbbreviationDescriptor',
+            'streetNumberName',
+            'nameOfCounty'
+        ],
         metaPrefix=None,
         recordPrefix=None,
         errors='ignore'
@@ -98,19 +137,20 @@ def contact_person_dim_dataframe(school_year) -> pd.DataFrame:
             'id',
             ['addresses', 'addressTypeDescriptor']
         ],
+        recordMeta=[
+            'beginDate'
+        ],
         metaPrefix=None,
         recordPrefix=None,
         errors='ignore'
     )
-
-    parents_address_normalize['_parents_address_periods'] = '|'
 
     parents_address_normalize = pdMerge(
         left=parents_address_normalize,
         right=parents_address_periods_normalize,
         how='left',
         leftOn=['id', 'addressTypeDescriptor'],
-        rigthOn=['id', 'addresses.addressTypeDescriptor'],
+        rightOn=['id', 'addresses.addressTypeDescriptor'],
         suffixLeft=None,
         suffixRight='_parents_address_periods'
     )
@@ -144,106 +184,94 @@ def contact_person_dim_dataframe(school_year) -> pd.DataFrame:
     # Parent Address - Home
     parents_address_normalize_home = (
         parents_address_normalize[
-            (parents_address_normalize['addressTypeDescriptor_constantName'].str.contains('Address.Home'))
+            (parents_address_normalize['addressTypeDescriptor_constantName'].str.contains('Address.Home', na=False))
         ]
     )
 
     # Parent Address - Physical
     parents_address_normalize_physical = (
         parents_address_normalize[
-            (parents_address_normalize['addressTypeDescriptor_constantName'].str.contains('Address.Physical'))
+            (parents_address_normalize['addressTypeDescriptor_constantName'].str.contains('Address.Physical', na=False))
         ]
     )
 
     # Parent Address - Mailing
     parents_address_normalize_mailing = (
         parents_address_normalize[
-            (parents_address_normalize['addressTypeDescriptor_constantName'].str.contains('Address.Mailing'))
+            (parents_address_normalize['addressTypeDescriptor_constantName'].str.contains('Address.Mailing', na=False))
         ]
     )
 
     # Parent Address - Work
     parents_address_normalize_work = (
         parents_address_normalize[
-            (parents_address_normalize['addressTypeDescriptor_constantName'].str.contains('Address.Work'))
+            (parents_address_normalize['addressTypeDescriptor_constantName'].str.contains('Address.Work', na=False))
         ]
     )
 
     # Parent Address - Temporary
     parents_address_normalize_temporary = (
         parents_address_normalize[
-            (parents_address_normalize['addressTypeDescriptor_constantName'].str.contains('Address.Temporary'))
+            (parents_address_normalize['addressTypeDescriptor_constantName'].str.contains('Address.Temporary', na=False))
         ]
     )
-
-    student_parent_associations_normalize['_parents'] = '|'
 
     result_data_frame = pdMerge(
         left=student_parent_associations_normalize,
         right=parents_normalize,
         how='inner',
         leftOn=['parentReference.parentUniqueId'],
-        rigthOn=['parentUniqueId'],
+        rightOn=['parentUniqueId'],
         suffixLeft=None,
         suffixRight='_parents'
     )
-
-    result_data_frame['address_home'] = '|'
 
     result_data_frame = pdMerge(
         left=result_data_frame,
         right=parents_address_normalize_home,
         how='left',
-        leftOn=['id_parents'],
-        rigthOn=['id'],
+        leftOn=['id'],
+        rightOn=['id'],
         suffixLeft=None,
         suffixRight='_address_home'
     )
-
-    result_data_frame['address_physical'] = '|'
 
     result_data_frame = pdMerge(
         left=result_data_frame,
         right=parents_address_normalize_physical,
         how='left',
-        leftOn=['id_parents'],
-        rigthOn=['id'],
+        leftOn=['id'],
+        rightOn=['id'],
         suffixLeft=None,
         suffixRight='_address_physical'
     )
-
-    result_data_frame['address_mailing'] = '|'
 
     result_data_frame = pdMerge(
         left=result_data_frame,
         right=parents_address_normalize_mailing,
         how='left',
-        leftOn=['id_parents'],
-        rigthOn=['id'],
+        leftOn=['id'],
+        rightOn=['id'],
         suffixLeft=None,
         suffixRight='_address_mailing'
     )
-
-    result_data_frame['address_work'] = '|'
 
     result_data_frame = pdMerge(
         left=result_data_frame,
         right=parents_address_normalize_work,
         how='left',
-        leftOn=['id_parents'],
-        rigthOn=['id'],
+        leftOn=['id'],
+        rightOn=['id'],
         suffixLeft=None,
         suffixRight='_address_work'
     )
-
-    result_data_frame['address_temporary'] = '|'
 
     result_data_frame = pdMerge(
         left=result_data_frame,
         right=parents_address_normalize_temporary,
         how='left',
-        leftOn=['id_parents'],
-        rigthOn=['id'],
+        leftOn=['id'],
+        rightOn=['id'],
         suffixLeft=None,
         suffixRight='_address_temporary'
     )
@@ -257,7 +285,6 @@ def contact_person_dim_dataframe(school_year) -> pd.DataFrame:
         'studentReference.studentUniqueId',
         'contactPriority',
         'contactRestrictions',
-        'id_parents',
         'parentUniqueId',
         'firstName',
         'lastSurname',
@@ -278,6 +305,10 @@ def contact_person_dim_dataframe(school_year) -> pd.DataFrame:
         meta=[
             'id'
         ],
+        recordMeta=[
+            'telephoneNumber',
+            'telephoneNumberTypeDescriptor'
+        ],
         metaPrefix=None,
         recordPrefix=None,
         errors='ignore'
@@ -289,54 +320,48 @@ def contact_person_dim_dataframe(school_year) -> pd.DataFrame:
 
     parents_telephones_normalize_home = (
         parents_telephones_normalize[
-            (parents_telephones_normalize['telephoneNumberTypeDescriptor_constantName'].str.contains('Telephone.Home'))
+            (parents_telephones_normalize['telephoneNumberTypeDescriptor_constantName'].str.contains('Telephone.Home', na=False))
         ]
     )
 
     parents_telephones_normalize_mobile = (
         parents_telephones_normalize[
-            (parents_telephones_normalize['telephoneNumberTypeDescriptor_constantName'].str.contains('Telephone.Mobile'))
+            (parents_telephones_normalize['telephoneNumberTypeDescriptor_constantName'].str.contains('Telephone.Mobile', na=False))
         ]
     )
 
     parents_telephones_normalize_work = (
         parents_telephones_normalize[
-            (parents_telephones_normalize['telephoneNumberTypeDescriptor_constantName'].str.contains('Telephone.Work'))
+            (parents_telephones_normalize['telephoneNumberTypeDescriptor_constantName'].str.contains('Telephone.Work', na=False))
         ]
     )
-
-    result_data_frame['phones_home'] = '|'
 
     result_data_frame = pdMerge(
         left=result_data_frame,
         right=parents_telephones_normalize_home,
         how='left',
-        leftOn=['id_parents'],
-        rigthOn=['id'],
+        leftOn=['id'],
+        rightOn=['id'],
         suffixLeft=None,
         suffixRight='_phones_home'
     )
-
-    result_data_frame['phones_mobile'] = '|'
 
     result_data_frame = pdMerge(
         left=result_data_frame,
         right=parents_telephones_normalize_mobile,
         how='left',
-        leftOn=['id_parents'],
-        rigthOn=['id'],
+        leftOn=['id'],
+        rightOn=['id'],
         suffixLeft=None,
         suffixRight='_phones_mobile'
     )
-
-    result_data_frame['phones_work'] = '|'
 
     result_data_frame = pdMerge(
         left=result_data_frame,
         right=parents_telephones_normalize_work,
         how='left',
-        leftOn=['id_parents'],
-        rigthOn=['id'],
+        leftOn=['id'],
+        rightOn=['id'],
         suffixLeft=None,
         suffixRight='_phones_work'
     )
@@ -350,7 +375,6 @@ def contact_person_dim_dataframe(school_year) -> pd.DataFrame:
         'studentReference.studentUniqueId',
         'contactPriority',
         'contactRestrictions',
-        'id_parents',
         'parentUniqueId',
         'firstName',
         'lastSurname',
@@ -374,6 +398,10 @@ def contact_person_dim_dataframe(school_year) -> pd.DataFrame:
         meta=[
             'id'
         ],
+        recordMeta=[
+            'electronicMailAddress',
+            'electronicMailTypeDescriptor'
+        ],
         metaPrefix=None,
         recordPrefix=None,
         errors='ignore'
@@ -387,36 +415,32 @@ def contact_person_dim_dataframe(school_year) -> pd.DataFrame:
 
     parents_electronicMails_normalize_personal = (
         parents_electronicMails_normalize[
-            (parents_electronicMails_normalize['electronicMailTypeDescriptor_constantName'].str.contains('Email.Personal'))
+            (parents_electronicMails_normalize['electronicMailTypeDescriptor_constantName'].str.contains('Email.Personal', na=False))
         ]
     )
 
     parents_electronicMails_normalize_work = (
         parents_electronicMails_normalize[
-            (parents_electronicMails_normalize['electronicMailTypeDescriptor_constantName'].str.contains('Email.Work'))
+            (parents_electronicMails_normalize['electronicMailTypeDescriptor_constantName'].str.contains('Email.Work', na=False))
         ]
     )
-
-    result_data_frame['_parents_mails_work'] = '|'
 
     result_data_frame = pdMerge(
         left=result_data_frame,
         right=parents_electronicMails_normalize_work,
         how='left',
-        leftOn=['id_parents'],
-        rigthOn=['id'],
+        leftOn=['id'],
+        rightOn=['id'],
         suffixLeft=None,
         suffixRight='_parents_mails_work'
     )
-
-    result_data_frame['_parents_mails_personal'] = '|'
 
     result_data_frame = pdMerge(
         left=result_data_frame,
         right=parents_electronicMails_normalize_personal,
         how='left',
-        leftOn=['id_parents'],
-        rigthOn=['id'],
+        leftOn=['id'],
+        rightOn=['id'],
         suffixLeft=None,
         suffixRight='_parents_mails_personal'
     )
@@ -443,7 +467,6 @@ def contact_person_dim_dataframe(school_year) -> pd.DataFrame:
         'studentReference.studentUniqueId',
         'contactPriority',
         'contactRestrictions',
-        'id_parents',
         'parentUniqueId',
         'firstName',
         'lastSurname',
@@ -493,35 +516,17 @@ def contact_person_dim_dataframe(school_year) -> pd.DataFrame:
         'postalCode': 'PostalCode'
     })
 
-    result_data_frame = result_data_frame[[
-        'UniqueKey',
-        'ContactPersonKey',
-        'StudentKey',
-        'ContactFirstName',
-        'ContactLastName',
-        'RelationshipToStudent',
-        'ContactHomeAddress',
-        'ContactPhysicalAddress',
-        'ContactMailingAddress',
-        'ContactWorkAddress',
-        'ContactTemporaryAddress',
-        'HomePhoneNumber',
-        'MobilePhoneNumber',
-        'WorkPhoneNumber',
-        'PrimaryEmailAddress',
-        'PersonalEmailAddress',
-        'WorkEmailAddress',
-        'IsPrimaryContact',
-        'StudentLivesWith',
-        'IsEmergencyContact',
-        'ContactPriority',
-        'ContactRestrictions',
-        'PostalCode'
-    ]]
+    result_data_frame = subset(result_data_frame, columns)
+
+    toCsv(result_data_frame, 'C:/temp/edfi/parquet/', 'contactPersonDim.csv', '')
+
 
     return result_data_frame
 
 
-def contact_person_dim(school_year) -> None:
-    result_data_frame = contact_person_dim_dataframe(school_year)
-    saveParquetFile(result_data_frame, f"{config('PARQUET_FILES_LOCATION')}", "contactPersonDim.parquet", school_year)
+def contact_person_dim(school_year) -> data_frame_generation_result:
+    return contact_person_dim_dataframe(
+        file_name="contactPersonDim.parquet",
+        columns=RESULT_COLUMNS,
+        school_year=school_year
+    )
