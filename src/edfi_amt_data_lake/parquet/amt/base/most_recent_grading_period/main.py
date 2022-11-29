420d0976d307a07eb3bccc41c602feee864080dc
José Alberto Leiva Obando <56046999+jleiva-gap@users.noreply.github.com>
2022-11-29 11:46:31 -0600

    [BIA-1278] Consider optional data - GradingPeriodDim (#69)
    Update GradingPeriodDim to validate optional data.
    Update MostRecentGradingPeriod to reuse GradingPeriodDim

--- src/edfi_amt_data_lake/parquet/amt/base/most_recent_grading_period/main.py
+++ src/edfi_amt_data_lake/parquet/amt/base/most_recent_grading_period/main.py
@@ -3,18 +3,45 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-from decouple import config
-
-from edfi_amt_data_lake.parquet.amt.base.grading_period_dim.main import (
-    grading_period_dim_dataframe,
+from edfi_amt_data_lake.helper.data_frame_generation_result import (
+    data_frame_generation_result,
 )
-from edfi_amt_data_lake.parquet.Common.pandasWrapper import saveParquetFile
+from edfi_amt_data_lake.parquet.amt.base.grading_period_dim.main import (
+    grading_period_dim,
+)
+from edfi_amt_data_lake.parquet.Common.pandasWrapper import create_parquet_file
+
+RESULT_COLUMNS = [
+    'GradingPeriodKey',
+    'GradingPeriodBeginDateKey',
+    'GradingPeriodEndDateKey',
+    'GradingPeriodDescription',
+    'TotalInstructionalDays',
+    'PeriodSequence',
+    'SchoolKey',
+    'SchoolYear'
+]
 
 
-def most_recent_grading_period(school_year) -> None:
-    result_data_frame = grading_period_dim_dataframe(school_year)
-
-    result_data_frame = result_data_frame.groupby(['schoolKey'], sort=False)['gradingPeriodBeginDateKey'].max()
+@create_parquet_file
+def most_recent_grading_period_dataframe(
+    file_name: str,
+    columns: list[str],
+    school_year: int
+):
+    file_name = file_name
+    columns = columns
+    result_data_frame = grading_period_dim(school_year).data_frame
+    if result_data_frame is None:
+        return None
+    result_data_frame = result_data_frame.groupby(['SchoolKey'], sort=False)['GradingPeriodBeginDateKey'].max()
     result_data_frame = result_data_frame.to_frame()
+    return result_data_frame
 
-    saveParquetFile(result_data_frame, f"{config('PARQUET_FILES_LOCATION')}", "mostRecentGradingPeriod.parquet", school_year)
+
+def most_recent_grading_period(school_year) -> data_frame_generation_result:
+    return most_recent_grading_period_dataframe(
+        file_name="mostRecentGradingPeriod.parquet",
+        columns=RESULT_COLUMNS,
+        school_year=school_year
+    )
