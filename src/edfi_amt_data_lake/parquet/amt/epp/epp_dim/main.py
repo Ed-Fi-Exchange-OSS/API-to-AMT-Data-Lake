db8333d9b0b189b4efc426f4e3beda79460794dc
José Alberto Leiva Obando <56046999+jleiva-gap@users.noreply.github.com>
2022-12-01 07:39:23 -0600

    [BIA-1270] Optional data: EPP_EppDim (#82)

--- src/edfi_amt_data_lake/parquet/amt/epp/epp_dim/main.py
+++ src/edfi_amt_data_lake/parquet/amt/epp/epp_dim/main.py
@@ -3,28 +3,43 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-import pandas as pd
 from decouple import config
 
+from edfi_amt_data_lake.helper.data_frame_generation_result import (
+    data_frame_generation_result,
+)
 from edfi_amt_data_lake.parquet.Common.functions import getEndpointJson
 from edfi_amt_data_lake.parquet.Common.pandasWrapper import (
+    create_parquet_file,
     get_descriptor_code_value_from_uri,
     jsonNormalize,
     pdMerge,
     renameColumns,
-    saveParquetFile,
 )
 
 ENDPOINT_SCHOOLS = 'schools'
+RESULT_COLUMNS = [
+    'EducationOrganizationKey',
+    'NameOfInstitution'
+]
 
 
-def epp_dim_dataframe(school_year) -> pd.DataFrame:
+@create_parquet_file
+def epp_dim_dataframe(
+    file_name: str,
+    columns: list[str],
+    school_year: int
+):
+    file_name = file_name
     epp_content = getEndpointJson(ENDPOINT_SCHOOLS, config('SILVER_DATA_LOCATION'), school_year)
 
     epp_normalize = jsonNormalize(
         epp_content,
         recordPath=None,
-        meta=None,
+        meta=[
+            'schoolId',
+            'nameOfInstitution'
+        ],
         metaPrefix=None,
         recordPrefix=None,
         errors='ignore'
@@ -35,6 +50,9 @@ def epp_dim_dataframe(school_year) -> pd.DataFrame:
         recordPath=['educationOrganizationCategories'],
         meta=[
             'schoolId'
+        ],
+        recordMeta=[
+            'educationOrganizationCategoryDescriptor'
         ],
         metaPrefix=None,
         recordPrefix=None,
@@ -50,10 +68,11 @@ def epp_dim_dataframe(school_year) -> pd.DataFrame:
         suffixLeft=None,
         suffixRight='_categories'
     )
-
+    if result_data_frame is None:
+        return None
     get_descriptor_code_value_from_uri(result_data_frame, 'educationOrganizationCategoryDescriptor')
 
-    result_data_frame = result_data_frame[result_data_frame['educationOrganizationCategoryDescriptor'].str.contains("Preparation Provider")]
+    result_data_frame = result_data_frame[result_data_frame['educationOrganizationCategoryDescriptor'].str.contains("Preparation Provider", na=False)]
 
     result_data_frame = result_data_frame[[
         "schoolId",
@@ -64,10 +83,13 @@ def epp_dim_dataframe(school_year) -> pd.DataFrame:
         'schoolId': 'EducationOrganizationKey',
         'nameOfInstitution': 'NameOfInstitution'
     })
-
+    result_data_frame = result_data_frame[columns]
     return result_data_frame
 
 
-def epp_dim(school_year) -> None:
-    result_data_frame = epp_dim_dataframe(school_year)
-    saveParquetFile(result_data_frame, f"{config('PARQUET_FILES_LOCATION')}", "epp_EppDim.parquet", school_year)
+def epp_dim(school_year) -> data_frame_generation_result:
+    return epp_dim_dataframe(
+        file_name="epp_EppDim.parquet",
+        columns=RESULT_COLUMNS,
+        school_year=school_year
+    )
