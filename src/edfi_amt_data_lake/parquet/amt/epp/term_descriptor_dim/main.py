5d7dbe22b40a4fb0b09bb51e8c0c49085d969f43
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2022-12-01 08:02:23 -0600

    Final details.

--- src/edfi_amt_data_lake/parquet/amt/epp/term_descriptor_dim/main.py
+++ src/edfi_amt_data_lake/parquet/amt/epp/term_descriptor_dim/main.py
@@ -3,8 +3,8 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-import pandas as pd
 from decouple import config
+
 from edfi_amt_data_lake.helper.data_frame_generation_result import (
     data_frame_generation_result,
 )
@@ -13,8 +13,8 @@ from edfi_amt_data_lake.parquet.Common.pandasWrapper import (
     create_parquet_file,
     jsonNormalize,
     renameColumns,
-    toCsv,
 )
+
 ENDPOINT_TERM_DESCRIPTOR = 'termDescriptors'
 RESULT_COLUMNS = [
     "TermDescriptorKey",
