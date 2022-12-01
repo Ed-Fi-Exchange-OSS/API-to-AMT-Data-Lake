2c8f1cb694116919d5219b5d32012f2b9569ed36
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2022-11-30 20:31:11 -0600

    [BIA-1271] Optional data:  epp_RaceDescriptorDim

--- src/edfi_amt_data_lake/parquet/amt/epp/race_descriptor_dim/main.py
+++ src/edfi_amt_data_lake/parquet/amt/epp/race_descriptor_dim/main.py
@@ -5,18 +5,29 @@
 
 import pandas as pd
 from decouple import config
-
+from edfi_amt_data_lake.helper.data_frame_generation_result import (
+    data_frame_generation_result,
+)
 from edfi_amt_data_lake.parquet.Common.functions import getEndpointJson
 from edfi_amt_data_lake.parquet.Common.pandasWrapper import (
+    create_parquet_file,
     jsonNormalize,
     renameColumns,
-    saveParquetFile,
 )
-
 ENDPOINT_RACE_DESCRIPTOR = 'raceDescriptors'
+RESULT_COLUMNS = [
+    "RaceDescriptorKey",
+    "CodeValue"
+]
 
 
-def race_descriptor_dim_dataframe(school_year) -> pd.DataFrame:
+@create_parquet_file
+def race_descriptor_dim_dataframe(
+    file_name: str,
+    columns: list[str],
+    school_year: int
+):
+    file_name = file_name
     race_descriptor_content = getEndpointJson(ENDPOINT_RACE_DESCRIPTOR, config('SILVER_DATA_LOCATION'), school_year)
 
     race_descriptor_normalize = jsonNormalize(
@@ -31,20 +42,18 @@ def race_descriptor_dim_dataframe(school_year) -> pd.DataFrame:
         recordPrefix=None,
         errors='ignore'
     )
-
     result_data_frame = renameColumns(race_descriptor_normalize, {
         'raceDescriptorId': 'RaceDescriptorKey',
         'codeValue': 'CodeValue'
     })
-
-    result_data_frame = result_data_frame[[
-        "RaceDescriptorKey",
-        "CodeValue"
-    ]]
-
+    result_data_frame = result_data_frame[columns]
     return result_data_frame
 
 
-def race_descriptor_dim(school_year) -> None:
-    result_data_frame = race_descriptor_dim_dataframe(school_year)
-    saveParquetFile(result_data_frame, f"{config('PARQUET_FILES_LOCATION')}", "epp_RaceDescriptorDim.parquet", school_year)
+def race_descriptor_dim(school_year) -> data_frame_generation_result:
+    return race_descriptor_dim_dataframe(
+        file_name="epp_RaceDescriptorDim.parquet",
+        columns=RESULT_COLUMNS,
+        school_year=school_year
+    )
+    
